a0d9044d3edd8e6bc038d0ae58a5a0c91e2163b5
Sohagmia4047 <sohagmia4047@gmail.com>
2026-07-04 09:47:31 +0400

    Update backend configuration more

--- backend/settings.py
+++ backend/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = 'django-insecure-%3el%2&etp$(qg&m!_4pae+yvrtv(f3pc&(qpwm!^hakwvbr8b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
